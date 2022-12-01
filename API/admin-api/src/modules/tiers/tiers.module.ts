92163613e3b3d26b7ee262b4d340b1319d999588
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-01 06:39:42 -0400

    modified tiers module

--- API/admin-api/src/modules/tiers/tiers.module.ts
+++ API/admin-api/src/modules/tiers/tiers.module.ts
@@ -5,10 +5,18 @@ import { TiersRepository } from 'src/repository/tiers.repository';
 import { LoanRepository } from 'src/repository/loan.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UnderwritingRepository } from 'src/repository/underwriting.repository';
+import { ReviewPlanRepository } from 'src/repository/reviewPlan.repository';
 
 @Module({
   controllers: [TiersController],
-  imports: [TypeOrmModule.forFeature([TiersRepository, LoanRepository, UnderwritingRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      TiersRepository,
+      LoanRepository,
+      UnderwritingRepository,
+      ReviewPlanRepository,
+    ]),
+  ],
   providers: [TiersService],
 })
 export class TiersModule {}
