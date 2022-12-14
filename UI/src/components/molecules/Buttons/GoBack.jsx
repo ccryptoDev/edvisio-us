74c8058119c8813f41da8a147106238bd5a94ecb
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-14 07:44:26 -0400

    updated goback

--- UI/src/components/molecules/Buttons/GoBack.jsx
+++ UI/src/components/molecules/Buttons/GoBack.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { ReactComponent as Chevron } from "../../../assets/svgs/chevron-left.svg";
-
-const Button = styled.div`
-  width: 36px;
-  height: 36px;
-
-  border: 1px solid var(--color-primary-green-1);
-  box-sizing: border-box;
-  border-radius: 50%;
-  background: #fff;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  cursor: pointer;
-  svg {
-    width: 6px;
-  }
-`;
+import { Rounded as Button } from "../../atoms/Buttons/Action";
 
 const GoBackButton = () => {
   const history = useHistory();
