40b85c2bbfd67c920cf62986948e2517afd49328
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-19 09:01:57 -0400

    modified modal

--- UI/src/components/organisms/Modal/Regular/index.tsx
+++ UI/src/components/organisms/Modal/Regular/index.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Dialog from "@material-ui/core/Dialog";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
 
 // THIS IS A REUSABLE MODAL COMPONENT:
 // YOU NEED TO CONNECT IT TO THE COMPONENT THAT IS TO TRIGGER IS
 // YOU CAN GO THE STORYBOOK TO SEE HOW IT IS USED
 
-const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 12px 12px 0 24px;
-`;
-
 const StyledDialog = styled(Dialog)`
   .MuiDialog-container .MuiDialog-paperWidthSm {
-    max-width: 800px;
+    max-width: 100%;
     min-height: 300px;
     margin-top: 10%;
 
@@ -46,56 +37,20 @@ const StyledDialog = styled(Dialog)`
   }
 `;
 
-const DialogTitle = ({
-  onClose,
-  children,
-}: {
-  onClose: any;
-  children: any;
-}) => {
-  return (
-    <TitleWrapper>
-      <h4> {children}</h4>
-      {onClose ? (
-        <IconButton aria-label="close" onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </TitleWrapper>
-  );
-};
-
 type IModalDialog = {
   open: boolean;
   modalContent: any;
-  modalTitle?: string;
-  showHideButton?: boolean;
-  state?: any;
-  cb?: Function;
   handleClose: any;
 };
 
-const ModalDialog = ({
-  modalContent: Content,
-  state,
-  cb,
-  open,
-  handleClose,
-  showHideButton = false,
-  modalTitle,
-}: IModalDialog) => {
+const ModalDialog = ({ modalContent, open, handleClose }: IModalDialog) => {
   return (
     <StyledDialog
       onClose={handleClose}
       aria-labelledby="customized-dialog-title"
       open={open}
     >
-      {modalTitle || showHideButton ? (
-        <DialogTitle onClose={handleClose}>{modalTitle}</DialogTitle>
-      ) : (
-        ""
-      )}
-      <Content closeModal={handleClose} state={state} cb={cb} />
+      {modalContent}
     </StyledDialog>
   );
 };
