a88fcf016b4a34d533664d6367778ed760f1bdf7
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-12-19 09:01:19 -0400

    imported new table

--- UI/src/components/organisms/Menu/TableRow/index.tsx
+++ UI/src/components/organisms/Menu/TableRow/index.tsx
@@ -1,52 +1,8 @@
 import React from "react";
-import { ReactComponent as Error } from "../../../../assets/svgs/error.svg";
-import { ReactComponent as Pencil } from "../../../../assets/svgs/pencil.svg";
-import { ReactComponent as Bin } from "../../../../assets/svgs/bin.svg";
-import { ReactComponent as Refresh } from "../../../../assets/svgs/refresh.svg";
-import { ReactComponent as Add } from "../../../../assets/svgs/add.svg";
-import { ReactComponent as View } from "../../../../assets/svgs/view.svg";
 import ActionButton from "../../../molecules/Buttons/ActionButtons";
 import Td from "./styles";
 
-const buttonName = {
-  VIEW: "view",
-  EDIT: "edit",
-  ADD: "add",
-  DELETE: "delete",
-  DISABLE: "disable",
-  ACTIVE: "active",
-};
-
-const buttons = [
-  {
-    label: buttonName.VIEW,
-    name: buttonName.VIEW,
-    icon: <View width="16px" />,
-  },
-  {
-    label: buttonName.EDIT,
-    name: buttonName.EDIT,
-    icon: <Pencil width="13px" />,
-  },
-  { label: buttonName.ADD, name: buttonName.ADD, icon: <Add width="15px" /> },
-  {
-    label: buttonName.DELETE,
-    name: buttonName.DELETE,
-    icon: <Bin width="18px" />,
-  },
-  {
-    label: buttonName.DISABLE,
-    name: buttonName.DISABLE,
-    icon: <Error width="15px" />,
-  },
-  {
-    label: buttonName.ACTIVE,
-    name: buttonName.ACTIVE,
-    icon: <Refresh width="12px" />,
-  },
-];
-
-const Menu = ({ onToggle, active, onClick }: any) => {
+const Menu = ({ onToggle, active, onClick, buttons = [] }: any) => {
   return (
     <Td className={`${active ? "expanded" : ""} menu-wrapper`}>
       <div className="cell">
@@ -54,7 +10,7 @@ const Menu = ({ onToggle, active, onClick }: any) => {
       </div>
       {active ? (
         <div className="menu">
-          {buttons.map((button) => {
+          {buttons.map((button: any) => {
             return (
               <button type="button" key={button.name} onClick={onClick}>
                 {button.icon}
