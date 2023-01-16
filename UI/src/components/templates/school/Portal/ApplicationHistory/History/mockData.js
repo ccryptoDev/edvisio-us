630e5afafb4040b3a0d99f6045bd8b55df39de9a
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-16 08:48:26 -0400

    modified application history table

--- UI/src/components/templates/school/Portal/ApplicationHistory/History/mockData.js
+++ UI/src/components/templates/school/Portal/ApplicationHistory/History/mockData.js
@@ -6,6 +6,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "1",
+      details: [
+        {
+          id: "11",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
     {
       date: "08/08/2022",
@@ -13,6 +23,16 @@ export const transactionsMock = {
       createdBy: "Claire Allen",
       link: "/school/application-history/12345",
       id: "2",
+      details: [
+        {
+          id: "21",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
   ],
   borrowerTransactions: [
@@ -22,6 +42,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "3",
+      details: [
+        {
+          id: "31",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
     {
       date: "08/08/2022",
@@ -29,6 +59,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "4",
+      details: [
+        {
+          id: "41",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
     {
       date: "08/08/2022",
@@ -36,6 +76,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "5",
+      details: [
+        {
+          id: "51",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
   ],
   coSignerTransactions: [
@@ -45,6 +95,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "6",
+      details: [
+        {
+          id: "61",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
     {
       date: "08/08/2022",
@@ -52,6 +112,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "7",
+      details: [
+        {
+          id: "71",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
     {
       date: "08/08/2022",
@@ -59,6 +129,16 @@ export const transactionsMock = {
       createdBy: "Website",
       link: "/school/application-history/12345",
       id: "8",
+      details: [
+        {
+          id: "81",
+          date: "08/08/2022",
+          type: "item",
+          details: "details",
+          createdBy: "Temeka Adams",
+          status: "Active",
+        },
+      ],
     },
   ],
 };
