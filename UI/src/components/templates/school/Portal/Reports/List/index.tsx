828b388f1644c2aead5260d8879863e360e40a2f
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-24 05:50:46 -0400

    report list in portal search

--- UI/src/components/templates/school/Portal/Reports/List/index.tsx
+++ UI/src/components/templates/school/Portal/Reports/List/index.tsx
@@ -1,51 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import {
   routes,
   pageName as reportNames,
 } from "../../../../../../routes/School/routes";
 import { ReactComponent as Logo } from "../../../../../../assets/svgs/list.svg";
-import { Text, Caption } from "../../../../../atoms/Typography";
+import ListButton from "../../../../../molecules/Buttons/ListBtn";
 
 const Wrapper = styled.div`
   ul {
     & li {
       list-style: none;
-      display: flex;
-      align-items: stretch;
-      box-shadow: var(--shadow-2);
-      border-radius: 4px;
       &:not(:last-child) {
         margin-bottom: 12px;
-      }
-
-      & .logo-wrapper {
-        padding: 25px;
-        display: flex;
-        align-items: center;
-        background: var(--color-bg-2);
-      }
-
-      & a {
-        text-decoration: none;
-        color: inherit;
-        font-weight: 600;
-        transition: all 0.3s;
-
-        &:hover {
-          text-decoration: underline;
-        }
-      }
-
-      & .content {
-        background: #fff;
-        padding: 16px;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        gap: 6px;
-        width: 100%;
       }
     }
   }
@@ -56,50 +23,48 @@ const renderReportsList = [
     title: "Applications Pending Certification",
     subtitle: "Submitted applications awaiting certification.",
     link: `${routes.REPORTS}/${reportNames.PENDING_CERTIFICATION}`,
+    logo: <Logo />,
   },
   {
     title: "Incomplete Applications",
     subtitle: "Applications started, but not completed.",
     link: `${routes.REPORTS}/${reportNames.INCOMPLETE_APPLICATIONS}`,
+    logo: <Logo />,
   },
   {
     title: "Applications Pending Esignature",
     subtitle: "Applications waiting for borrower/cosigner esignature.",
     link: `${routes.REPORTS}/${reportNames.PENDING_SIGNATURE}`,
+    logo: <Logo />,
   },
   {
     title: "Processed School Certifications",
     subtitle: "All applications certified through the school portal.",
     link: `${routes.REPORTS}/${reportNames.PROCESSED_CERTIFICATIONS}`,
+    logo: <Logo />,
   },
   {
     title: "School Pre-Approval Report",
     subtitle: `All applications in "School Pre-Approval Incomplete" status.`,
     link: `${routes.REPORTS}/${reportNames.PRE_APPROVAL}`,
+    logo: <Logo />,
   },
   {
     title: "Application All-Inclusive Report",
     subtitle: "All applications, all statuses.",
     link: `${routes.REPORTS}/${reportNames.ALL_INCLUSIVE}`,
+    logo: <Logo />,
   },
 ];
 
-const Application = () => {
+const ReportsList = () => {
   return (
     <Wrapper>
       <ul>
         {renderReportsList.map((item) => {
           return (
             <li key={item.link}>
-              <div className="logo-wrapper">
-                <Logo />
-              </div>
-              <div className="content">
-                <Text className="title">
-                  <Link to={item.link}>{item.title}</Link>
-                </Text>
-                <Caption className="subtitle">{item.subtitle}</Caption>
-              </div>
+              <ListButton {...item} />
             </li>
           );
         })}
@@ -108,4 +73,4 @@ const Application = () => {
   );
 };
 
-export default Application;
+export default ReportsList;
