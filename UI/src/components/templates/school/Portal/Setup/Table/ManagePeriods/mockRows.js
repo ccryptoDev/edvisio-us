c63d6c1f5ca957f263521390879bced124501997
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-01-30 09:54:25 -0400

    removed previous managerPeriod mockup

--- UI/src/components/templates/school/Portal/Setup/Table/ManagePeriods/mockRows.js
+++ UI/src/components/templates/school/Portal/Setup/Table/ManagePeriods/mockRows.js
@@ -1,18 +1,20 @@
 import { v4 as uuid } from "uuid";
 
 export const createRows = Array(32).fill({
-  school: "Ed2go",
-  userLevel: "Demo School",
-  organization: "SELF",
-  product: "TuitionEase",
-  status: "Pending certification",
-  updatedAt: "09/09/2022",
-  amountRequested: 12700,
-  amountCertified: 12700,
-  creditDebt: 12000,
+  name: "Business Coach",
+  description: "Class 17",
+  startDate: "01/28/2023",
+  endDate: "09/09/2023",
 });
 
 export const rows = createRows.map((row) => ({
   ...row,
   screenTracking: uuid(),
 }));
+
+export const schoolsOptions = [
+  { label: "", value: "", id: "" },
+  { label: "Abc School", value: "Abc school", id: "1" },
+  { label: "College", value: "College", id: "2" },
+  { label: "Lambda School", value: "Lambda School", id: "3" },
+];
