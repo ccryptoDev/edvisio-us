bf700e93c1fce63e30a7c0e0143b8c63f1c0395b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-02-02 13:09:23 -0400

    manageUsers setup table

--- UI/src/components/templates/school/Portal/Setup/Table/ManageUsers/index.jsx
+++ UI/src/components/templates/school/Portal/Setup/Table/ManageUsers/index.jsx
@@ -1,29 +1,80 @@
 /*eslint-disable*/
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Table from "../../../../../../organisms/Tables/Default";
+import Table from "../../../../../../organisms/Tables/Paginated";
 import Rows from "./Rows";
 import Thead from "./Thead";
-import { fetchTable } from "../../../../../../organisms/Tables/mockApi";
+import { fetchTable as api } from "../../../../../../organisms/Tables/mockApi";
 import { ClassicTable } from "../../../../../../atoms/Table";
+import { tableRowChessBackground } from "../../../../../../atoms/Table/Elements";
 import { rows } from "./mockRows";
+import Card from "../../../../../../atoms/Cards";
+import Header from "./Header";
+import Modal from "../../../../../../organisms/Modal/Regular";
+import Form from "./Form";
+import { usePaginatedTable } from "../../../../../../organisms/Tables/table-hook";
+import Loader from "../../../../../../molecules/Loaders/LoaderWrapper";
+
+const Wrapper = styled.div`
+  .card {
+    padding: 12px;
+    border-radius: 4px;
+  }
+`;
 
 const TableWrapper = styled(ClassicTable)`
   border-radius: 12px;
   border: 1px solid var(--color-gray-3);
-  overflow: hidden;
+  border-radius: 4px;
+  ${tableRowChessBackground}
+
+  tbody tr td, {
+    border-bottom: 1px solid var(--color-gray-3);
+  }
+
+  thead tr th {
+    border-bottom: 1px solid var(--color-gray-3);
+    border-top: 1px solid var(--color-gray-3);
+  }
 `;
 
 const TableControls = () => {
+  const [open, setOpen] = useState(false);
+  const { tableData, loading, fetchTable, pagination } = usePaginatedTable({
+    api,
+    itemsPerPage: 15,
+    payload: { page: 1, mockRows: rows },
+  });
+
+  useEffect(() => {
+    fetchTable();
+  }, []);
+
+  const closeModalHandler = () => setOpen(false);
+
   return (
-    <TableWrapper>
-      <Table
-        api={fetchTable}
-        payload={{ mockRows: rows }}
-        rows={Rows}
-        thead={Thead}
-      />
-    </TableWrapper>
+    <Wrapper>
+      <Card className="card">
+        <TableWrapper>
+          <Header onClick={() => setOpen(true)} />
+          <Loader loading={loading}>
+            <Table
+              rows={<Rows items={tableData?.items} cb={fetchTable} />}
+              thead={<Thead />}
+              pagination={pagination}
+              loading={loading}
+            />
+          </Loader>
+        </TableWrapper>
+        <Modal
+          modalTitle="Add User"
+          showHideButton
+          open={open}
+          modalContent={<Form closeModal={closeModalHandler} cb={fetchTable} />}
+          handleClose={closeModalHandler}
+        />
+      </Card>
+    </Wrapper>
   );
 };
 
