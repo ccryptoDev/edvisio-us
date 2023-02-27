c7baf05f66cea7d7be794373c313a0ea07a13e37
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2023-02-27 05:37:45 -0400

    setup portal managerusers

---
+++ UI/src/pages/school/portal/Setup/ManageUsers.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { H5 } from "../../../../components/atoms/Typography";
+import Layout from "../../../../layouts/school/Portal";
+import { routes } from "../../../../routes/School/routes";
+import Table from "../../../../components/templates/school/Portal/Setup/Table/ManageUsers";
+
+const Wrapper = styled.div`
+  padding: 24px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  .table-wrapper-card {
+    padding: 12px;
+  }
+`;
+
+const ManageUsers = () => {
+  return (
+    <Layout currentRoute={routes.SETUP}>
+      <Wrapper>
+        <H5>Setup</H5>
+        <Table />
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default ManageUsers;
